8edccdb23aac6659f52a12c5b427fa7009337597
Mirko Sertic <mirko.sertic@web.de>
2013-11-10 11:39:44 +0100

    Minor enhancements to event system

--- gameengine/src/main/java/de/mirkosertic/gameengine/event/Property.java
+++ gameengine/src/main/java/de/mirkosertic/gameengine/event/Property.java
@@ -1,19 +1,22 @@
 package de.mirkosertic.gameengine.event;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public abstract class Property<T> {
 
     private String name;
     private T value;
     private Object owner;
-    private GameEventListener<PropertyChangeEvent> changeListener;
+    private Set<GameEventListener<PropertyChangeEvent>> changeListener;
 
     Property(Object aOwner, String aName, T aDefaultValue, GameEventListener<PropertyChangeEvent> aListener) {
+        changeListener = new HashSet<GameEventListener<PropertyChangeEvent>>();
         owner = aOwner;
         name = aName;
-        changeListener = aListener;
+        changeListener.add(aListener);
         value = aDefaultValue;
     }
-
 
     Property(Object aOwner, String aName, GameEventListener<PropertyChangeEvent> aListener) {
         this(aOwner, aName, null, aListener);
@@ -36,10 +39,20 @@ public abstract class Property<T> {
     public void set(T aValue) {
         T theOldValue = value;
         value = aValue;
-        changeListener.handleGameEvent(new PropertyChangeEvent(this, theOldValue));
+        for (GameEventListener<PropertyChangeEvent> theListener : changeListener) {
+            theListener.handleGameEvent(new PropertyChangeEvent(this, theOldValue));
+        }
     }
 
     public void setQuietly(T aValue) {
         value = aValue;
+    }
+
+    public void addChangeListener(GameEventListener<PropertyChangeEvent> aListener) {
+        changeListener.add(aListener);
+    }
+
+    public void removeChangeListener(GameEventListener<PropertyChangeEvent> aListener) {
+        changeListener.remove(aListener);
     }
 }
