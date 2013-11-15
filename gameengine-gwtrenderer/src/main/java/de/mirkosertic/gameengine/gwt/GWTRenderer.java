3129b82134db20f23aa9ae129b87f99879607cec
Mirko Sertic <mirko.sertic@web.de>
2013-11-15 22:02:41 +0100

    Last fixes

--- gameengine-gwtrenderer/src/main/java/de/mirkosertic/gameengine/gwt/GWTRenderer.java
+++ gameengine-gwtrenderer/src/main/java/de/mirkosertic/gameengine/gwt/GWTRenderer.java
@@ -9,6 +9,7 @@ import com.google.gwt.core.client.EntryPoint;
 import com.google.gwt.event.dom.client.*;
 import com.google.gwt.event.logical.shared.ResizeEvent;
 import com.google.gwt.event.logical.shared.ResizeHandler;
+import com.google.gwt.user.client.Event;
 import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.RootPanel;
