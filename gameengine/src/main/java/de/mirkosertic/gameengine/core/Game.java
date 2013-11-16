2937e4cb01b197957b3fe6b8f64c5dc4d3b34667
Mirko Sertic <mirko.sertic@web.de>
2013-11-16 17:13:41 +0100

    Removed list of scenes from game.json, as this can easily be retrieved from game directory structure.

--- gameengine/src/main/java/de/mirkosertic/gameengine/core/Game.java
+++ gameengine/src/main/java/de/mirkosertic/gameengine/core/Game.java
@@ -2,37 +2,27 @@ package de.mirkosertic.gameengine.core;
 
 import de.mirkosertic.gameengine.event.Property;
 
-import java.util.*;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Game {
 
     private final Property<String> name;
-    private final Set<String> scenes;
     private final Property<String> defaultScene;
 
     public Game() {
         name = new Property<String>(this, "name", (String) null);
         defaultScene = new Property<String>(this, "defaultScene", (String) null);
-        scenes = new HashSet<String>();
     }
 
     public Property<String> nameProperty() {
         return name;
     }
 
-    public void addScene(String aSceneName) {
-        scenes.add(aSceneName);
-    }
-
     public void removeScene(String aSceneName) {
-        scenes.remove(aSceneName);
         if (aSceneName.equals(defaultScene.get())) {
             defaultScene.set(null);
         }
-    }
-
-    public Set<String> getScenes() {
-        return Collections.unmodifiableSet(scenes);
     }
 
     public Property<String> defaultSceneProperty() {
@@ -42,7 +32,6 @@ public class Game {
     public Map<String, Object> serialize() {
         Map<String, Object> theResult = new HashMap<String, Object>();
         theResult.put("name", name.get());
-        theResult.put("scenes", new ArrayList<String>(scenes));
         theResult.put("defaultscene", defaultScene.get());
         return theResult;
     }
@@ -51,12 +40,6 @@ public class Game {
         Game theResult = new Game();
         theResult.name.setQuietly((String) aSerializedData.get("name"));
         theResult.defaultScene.setQuietly((String) aSerializedData.get("defaultscene"));
-
-        Collection<String> theScenes = (Collection<String>) aSerializedData.get("scenes");
-        for (String theScene : theScenes) {
-            theResult.addScene(theScene);
-        }
-
         return theResult;
     }
 }
