c583f5f645e3582f5850f3f80ab224ef683f9e73
Fernando Petrola <fernando.petrola@dragome.com>
2014-11-22 22:00:31 -0300

    Wrapper for Runnables added: it allows remote debugging in Dragome when you use Runnable as a javascript callback.

--- gameengine-dragomerenderer/src/main/java/de/mirkosertic/gameengine/dragome/DragomeBuildInFunctions.java
+++ gameengine-dragomerenderer/src/main/java/de/mirkosertic/gameengine/dragome/DragomeBuildInFunctions.java
@@ -1,9 +1,15 @@
 package de.mirkosertic.gameengine.dragome;
 
-import de.mirkosertic.gameengine.expression.BuiltInFunctions;
-
+import java.io.Serializable;
 import java.sql.Timestamp;
 import java.text.SimpleDateFormat;
+
+import de.mirkosertic.gameengine.expression.BuiltInFunctions;
+
+import com.dragome.commons.javascript.ScriptHelper;
+import com.dragome.remote.entities.DragomeEntityManager;
+import com.dragome.services.RequestExecutorImpl;
+import com.dragome.services.interfaces.AsyncCallback;
 
 public class DragomeBuildInFunctions extends BuiltInFunctions {
 
@@ -12,4 +18,35 @@ public class DragomeBuildInFunctions extends BuiltInFunctions {
         SimpleDateFormat theFormat = new SimpleDateFormat(aPattern);
         return theFormat.format(new Timestamp(aTimeInMilis.longValue()));
     }
+    
+    public static Runnable wrapRunnableForDebugging(final Runnable runnable)
+    {
+	AsyncCallback<String> asyncCallback= new AsyncCallback<String>()
+	{
+	    public void onSuccess(String result)
+	    {
+		runnable.run();
+	    }
+
+	    public void onError()
+	    {
+	    }
+	};
+
+	AsyncCallback<String> wrappedCallback= RequestExecutorImpl.wrapCallback(Serializable.class, asyncCallback);
+
+	Runnable runnable2= new Runnable()
+	{
+	    public void run()
+	    {
+		ScriptHelper.eval("window['callback_'+this.javaId].$onSuccess___java_lang_Object$void(new String())", this);
+	    }
+	};
+	
+	ScriptHelper.put("callback", wrappedCallback, null);
+	ScriptHelper.put("javaId", DragomeEntityManager.getEntityId(runnable2), null);
+	ScriptHelper.eval("window['callback_'+javaId]= callback", null);
+	return runnable2;
+    }
 }
+
