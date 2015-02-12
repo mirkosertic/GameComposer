a526dcb8ea78a5f50cc293a334382dd13b553553
Mirko Sertic <mirko.sertic@web.de>
2015-02-12 14:57:57 +0100

    Working on #12

--- gamecomposer/src/main/java/de/mirkosertic/gamecomposer/GameComposerFactory.java
+++ gamecomposer/src/main/java/de/mirkosertic/gamecomposer/GameComposerFactory.java
@@ -14,6 +14,7 @@ import javax.enterprise.inject.Produces;
 import javax.inject.Inject;
 import java.io.IOException;
 import java.io.InputStream;
+import java.net.URL;
 import java.util.ResourceBundle;
 
 public class GameComposerFactory {
@@ -32,7 +33,16 @@ public class GameComposerFactory {
             ResourceBundle theBundle = ResourceBundle.getBundle("de.mirkosertic.gamecomposer.GameComposer");
             theLoader.setResources(theBundle);
             Region root = theLoader.load(fxml);
-            root.getStylesheets().add("/JMetroLightTheme.css");
+
+            URL theURL = GameComposerFactory.class.getResource("/fxstyle.css");
+            root.getStylesheets().add(theURL.toExternalForm());
+
+            if (System.getProperties().get("devmode") != null) {
+                // Enable reloading of stylesheets during development
+                StylesheetReloaderThread theReloadThread = new StylesheetReloaderThread(root, theURL);
+                theReloadThread.start();
+            }
+
             ((GameComposerController)theLoader.getController()).initialize(aStage);
 
             UndecoratorScene theUndecoratorScene = new UndecoratorScene(aStage, root);
