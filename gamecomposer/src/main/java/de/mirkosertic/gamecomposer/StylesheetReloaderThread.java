787e52e3d3431bf1a2ee249a1181ad88dcb67c92
sertic <sertic@buch.de>
2015-02-12 14:57:57 +0100

    Working on #12

---
+++ gamecomposer/src/main/java/de/mirkosertic/gamecomposer/StylesheetReloaderThread.java
@@ -0,0 +1,49 @@
+package de.mirkosertic.gamecomposer;
+
+import javafx.application.Platform;
+import javafx.scene.layout.Region;
+
+import java.io.IOException;
+import java.net.URL;
+import java.net.URLConnection;
+
+public class StylesheetReloaderThread extends Thread {
+
+    private final Region region;
+    private final URL urlToWatch;
+
+    public StylesheetReloaderThread(Region aRegion, URL aUrlToWatch) {
+        region = aRegion;
+        urlToWatch = aUrlToWatch;
+
+        setDaemon(true);
+        setName("DEV Stylesheetreloader");
+    }
+
+    @Override
+    public void run() {
+        long theLastModified = 0;
+        while (!interrupted()) {
+            try {
+                URLConnection theConnection = urlToWatch.openConnection();
+                long theCurrentModified = theConnection.getLastModified();
+                if (!(theCurrentModified == theLastModified)) {
+
+                    Platform.runLater(new Runnable() {
+                        @Override
+                        public void run() {
+                            region.getStylesheets().clear();
+                            region.getStylesheets().add(urlToWatch.toExternalForm());
+                        }
+                    });
+
+                    theLastModified = theCurrentModified;
+                }
+
+                Thread.sleep(1000);
+            } catch (Exception e) {
+                // Should never happen
+            }
+        }
+    }
+}
