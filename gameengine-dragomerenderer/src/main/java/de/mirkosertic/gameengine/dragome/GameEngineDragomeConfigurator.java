b536de1e19e2efa38be03c1d6cfafa23f2956d08
Fernando Petrola <fernando.petrola@dragome.com>
2016-07-01 23:50:08 -0300

    adapting configuration to 0.96-beta3 version

--- gameengine-dragomerenderer/src/main/java/de/mirkosertic/gameengine/dragome/GameEngineDragomeConfigurator.java
+++ gameengine-dragomerenderer/src/main/java/de/mirkosertic/gameengine/dragome/GameEngineDragomeConfigurator.java
@@ -1,62 +1,45 @@
 package de.mirkosertic.gameengine.dragome;
 
+import java.net.URL;
+
 import com.dragome.commons.DragomeConfiguratorImplementor;
 import com.dragome.commons.compiler.CompilerMode;
 import com.dragome.web.config.DomHandlerApplicationConfigurator;
-import com.dragome.web.helpers.serverside.DefaultClasspathFilter;
-
-import java.io.File;
 
 @DragomeConfiguratorImplementor
 public class GameEngineDragomeConfigurator extends DomHandlerApplicationConfigurator {
 
     public GameEngineDragomeConfigurator() {
-        System.setProperty("dragome-compile-mode", CompilerMode.Production.toString());
-
-        setClasspathFilter(new DefaultClasspathFilter() {
-            @Override
-            public boolean accept(File pathname, File aFolder) {
-                boolean accept = super.accept(pathname, aFolder);
-
-                String string = pathname.toString();
-
-                accept &= !string.contains("java/util/concurrent");
-                accept &= !string.contains("java/util/stream");
-                accept &= !string.contains("java/util/function");
-                accept &= !string.contains("java/sql");
-                accept &= !string.contains("org/w3c/dom/html");
-                accept &= !string.contains("java/org/junit");
-                accept &= !string.contains("junit");
-
-                return accept;
-            }
-        });
+        System.setProperty("dragome-compile-mode", CompilerMode.Debug.toString());
     }
 
     @Override
     public boolean filterClassPath(String aClassPathEntry) {
-        if (aClassPathEntry.contains("gameengine-0.5"))
-            return true;
-        if (aClassPathEntry.contains("gameengine-lua"))
-            return true;
-        if (aClassPathEntry.contains("gameengine-jbox2d"))
-            return true;
-        if (aClassPathEntry.contains("dragome-js-jre"))
-            return true;
-        if (aClassPathEntry.contains("dragome-w3c"))
-            return true;
-        if (aClassPathEntry.contains("dragome-web"))
-            return true;
-        if (aClassPathEntry.contains("dragome-js-commons"))
-            return true;
-        if (aClassPathEntry.contains("dragome-core"))
-            return true;
+		boolean include= false;
 
-        return false;
+		include|= aClassPathEntry.contains("gameengine-0.5");
+		include|= aClassPathEntry.contains("gameengine-lua");
+		include|= aClassPathEntry.contains("gameengine-jbox2d");
+		include|= aClassPathEntry.contains("dragome-js-jre");
+		include|= aClassPathEntry.contains("dragome-web");
+		include|= aClassPathEntry.contains("dragome-w3c");
+		include|= aClassPathEntry.contains("dragome-js-commons");
+		include|= aClassPathEntry.contains("dragome-core");
+
+		return include;
     }
 
     @Override
     public boolean isCheckingCast() {
         return false;
+    }
+
+    public boolean isRemoveUnusedCode()
+    {
+        return true;
+    }
+    
+    public URL getAdditionalCodeKeepConfigFile() {
+    	return getClass().getResource("/gameengine-proguard.conf");
     }
 }
