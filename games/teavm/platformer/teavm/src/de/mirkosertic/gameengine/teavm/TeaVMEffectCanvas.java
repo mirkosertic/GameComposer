3a580f83e047469e77ea7e6ab22686baf0ae2237
mirkosertic <mirko.sertic@web.de>
2016-07-28 18:54:18 +0200

    New examples build

--- games/teavm/platformer/teavm/src/de/mirkosertic/gameengine/teavm/TeaVMEffectCanvas.java
+++ games/teavm/platformer/teavm/src/de/mirkosertic/gameengine/teavm/TeaVMEffectCanvas.java
@@ -34,14 +34,47 @@ public class TeaVMEffectCanvas implements EffectCanvas {
     }
 
     @Override
-    public void fillPolygon(double[] aXPositions, double[] aYPositions, int aNumberOfPositions) {
+    public void fillTriangle(double aX0, double aY0, double aX1, double aY1, double aX2, double aY2) {
         context.beginPath();
-        context.moveTo(aXPositions[0], aYPositions[0]);
-        for (int i=1;i<aNumberOfPositions;i++) {
-            context.lineTo(aXPositions[i], aYPositions[i]);
-        }
+        context.moveTo(aX0, aY0);
+        context.lineTo(aX1, aY1);
+        context.lineTo(aX2, aY2);
         context.closePath();
         context.fill();
+        context.stroke();
+    }
+
+    @Override
+    public void fillTriangle(GameResource aTexture, double aX0, double aY0, double aX1, double aY1, double aX2,
+            double aY2, double aU0, double aV0, double aU1, double aV1, double aU2, double aV2) {
+
+        // Affine Texture Mapping
+        context.save();
+
+        context.beginPath();
+        context.moveTo(aX0, aY0);
+        context.lineTo(aX1, aY1);
+        context.lineTo(aX2, aY2);
+        context.closePath();
+        context.clip();
+
+        aX1 -= aX0; aY1 -= aY0; aX2 -= aX0; aY2 -= aY0;
+        aU1 -= aU0; aV1 -= aV0; aU2 -= aU0; aV2 -= aV0;
+
+        double id = 1.0 / (aU1*aV2 - aU2*aV1);
+        double a = id * (aV2*aX1 - aV1*aX2);
+        double b = id * (aV2*aY1 - aV1*aY2);
+        double c = id * (aU1*aX2 - aU2*aX1);
+        double d = id * (aU1*aY2 - aU2*aY1);
+        double e = aX0 - a*aU0 - c*aV0;
+        double f = aY0 - b*aU0 - d*aV0;
+
+        context.transform( a, b, c, d, e, f );
+        TeaVMGameResource theBitmap = (TeaVMGameResource) aTexture;
+        context.drawImage(theBitmap.getElement(), 0, 0);
+
+        context.restore();
+
     }
 
     @Override
