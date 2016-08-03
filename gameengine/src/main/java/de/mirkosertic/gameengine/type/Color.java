6a42787d7490746951dd768ccaa7427fda7cdbb4
sertic <m.sertic@tde.thalia.de>
2016-08-03 09:28:43 +0200

    Performance tuning

--- gameengine/src/main/java/de/mirkosertic/gameengine/type/Color.java
+++ gameengine/src/main/java/de/mirkosertic/gameengine/type/Color.java
@@ -11,6 +11,8 @@ public class Color implements Reflectable<ColorClassInformation>, Distributable 
 
     public static final Color WHITE = new Color(255, 255, 255);
     public static final Color BLACK = new Color(0, 0, 0);
+    public static final Color RED = new Color(255, 0, 0);
+    public static final Color YELLOW = new Color(255, 255, 0);
 
     @ReflectiveField
     public final int r;
