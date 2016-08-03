f4e78f6e6979157f5b4bc1465d71fb5ee2422111
Mirko Sertic <mirko.sertic@web.de>
2016-08-03 11:01:54 +0200

    Performance tuning

--- gameengine/src/main/java/de/mirkosertic/gameengine/type/EffectCanvas.java
+++ gameengine/src/main/java/de/mirkosertic/gameengine/type/EffectCanvas.java
@@ -8,5 +8,5 @@ public interface EffectCanvas {
 
     void fillRectangle(String aObjectIdentifier, int aX0, int aY0, int aX1, int aY1, int aX2, int aY2, int aX3, int aY3, Color aColor, int aZIndex);
 
-    void fillRectangle(String aObjectIdentifier, GameResource aTexture, int aX0, int aY0, int aX1, int aY1, int aX2, int aY2, int aX3, int aY3, int aU0, int aV0, int aU1, int aV1, int aU2, int aV2, int aU3, int aV3, int aZIndex);
+    void fillRectangle(String aObjectIdentifier, GameResource aTexture, int aX0, int aY0, int aX1, int aY1, int aX2, int aY2, int aX3, int aY3, double aU0, double aV0, double aU1, double aV1, double aU2, double aV2, double aU3, double aV3, int aZIndex);
 }
