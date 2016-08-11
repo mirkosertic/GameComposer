bdf1e7ed8855411285233c96b9b688d82736f790
Mirko Sertic <mirko.sertic@web.de>
2016-08-11 20:39:25 +0200

    Working on Github API

--- gameengine-editor/src/main/java/de/mirkosertic/gameengine/web/github/Repository.java
+++ gameengine-editor/src/main/java/de/mirkosertic/gameengine/web/github/Repository.java
@@ -1,6 +1,18 @@
 package de.mirkosertic.gameengine.web.github;
 
 import org.teavm.jso.JSObject;
+import org.teavm.jso.JSProperty;
 
 public interface Repository extends JSObject {
+
+    @JSProperty
+    long getId();
+
+    @JSProperty
+    String getName();
+
+    @JSProperty
+    String getFull_name();
+
+    void getCommit(String aSHA, Callback<Commit> aCallback);
 }
--- gameengine-teavmcommon/src/main/java/de/mirkosertic/gameengine/teavm/TeaVMGameLoader.java
+++ gameengine-teavmcommon/src/main/java/de/mirkosertic/gameengine/teavm/TeaVMGameLoader.java
@@ -1,11 +1,10 @@
 package de.mirkosertic.gameengine.teavm;
 
 import de.mirkosertic.gameengine.core.Game;
-
-import java.util.Map;
-import org.teavm.jso.ajax.ReadyStateChangeHandler;
 import org.teavm.jso.ajax.XMLHttpRequest;
 import org.teavm.jso.json.JSON;
+
+import java.util.Map;
 
 public class TeaVMGameLoader {
 
@@ -24,22 +23,12 @@ public class TeaVMGameLoader {
         final XMLHttpRequest theRequest = XMLHttpRequest.create();
         theRequest.overrideMimeType("text/plain");
         theRequest.open("GET", "game.json");
-        theRequest.setOnReadyStateChange(new ReadyStateChangeHandler() {
-            @Override
-            public void stateChanged() {
-                switch (theRequest.getReadyState()) {
-                    default:
-                        break;
-                    case XMLHttpRequest.DONE:
-                        listener.onGameLoaded(parse(theRequest.getResponseText()));
-                }
-            }
-        });
+        theRequest.onComplete(() -> listener.onGameLoaded(parse(theRequest.getResponseText())));
         theRequest.send();
     }
 
     private Game parse(String aResponse) {
-        Map<String, Object> theResult = new TeaVMMap((TeaVMMap.JSDelegate) JSON.parse(aResponse).cast());
+        Map<String, Object> theResult = new TeaVMMap(JSON.parse(aResponse).cast());
         return Game.deserialize(theResult);
     }
 }
