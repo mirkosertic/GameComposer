962df9187d20053e32cfc381246568065788de62
Mirko Sertic <mirko.sertic@web.de>
2016-11-11 17:13:42 +0100

    #28 Optimize callbacks to make them compatible with Promise API

--- gameengine-editor/src/main/java/de/mirkosertic/gameengine/web/github/GithubEditorProject.java
+++ gameengine-editor/src/main/java/de/mirkosertic/gameengine/web/github/GithubEditorProject.java
@@ -22,6 +22,7 @@ public class GithubEditorProject implements EditorProject {
 
     // http://mdswanson.com/blog/2011/07/23/digging-around-the-github-api-take-2.html
     // https://gist.github.com/robnyman/1894032
+    // https://api.github.com/repos/mirkosertic/GameComposer/git/refs/heads/master
 
     private final String username;
     private final String repository;
